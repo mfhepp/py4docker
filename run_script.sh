a5f5736567b0a748ef34d8c2e846d78d0f891aa5
mfhepp <mfhepp@users.noreply.github.com>
2023-11-30 23:12:08 +0100

    no new kernel capabilities

--- run_script.sh
+++ run_script.sh
@@ -9,6 +9,7 @@ docker run \
  --mount type=bind,source="$(pwd)/output",target=/usr/app/src/output \
  --net none \
  --security-opt seccomp=seccomp-default.json \
+ --security-opt=no-new-privileges \
  --read-only --tmpfs /tmp \
  --cap-drop all \
  --rm \
